b4c56979f0ee3b5c810156ff0a93737e2f54e035
vinayakz <vinoo160496@gmail.com>
2022-07-22 21:18:48 +0530

    Terraform scripts

---
+++ Firstscript/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.16.0"
+    }
+  }
+}
+
+# Download the latest Ghost image
+resource "docker_image" "image_id1" {
+  name = "ghost:latest"
+}
