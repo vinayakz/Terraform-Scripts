fcc778661be2ab92eaffa9830644d1ea17e9fa0c
Vinayak Zutti <33689324+vinayakz@users.noreply.github.com>
2022-07-25 14:23:37 +0530

    Update main.tf

--- consoleOutputs/main.tf
+++ consoleOutputs/main.tf
@@ -12,24 +12,3 @@ resource "docker_container" "container_id" {
     external = "80"
   }
 }
-
-#Output the IP Address of the Container
-output "IP_Address" {
-  value = docker_container.container_id.ip_address
-}
-
-output "container_name" {
-  value = docker_container.container_id.name
-}
-
-output "dockerimage_name" {
-  value = docker_image.image_id.name
-}
-output "container_must_run" {
-  value = docker_container.container_id.must_run
-  sensitive = true
-}
-output "url" {
-  description = "Browser URL for container site"
-  value       = join(":", ["http://localhost", tostring(docker_container.container_id.ports[0].external)])
-}
